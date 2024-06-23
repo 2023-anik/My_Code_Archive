83c6692003dfca1300077cd78ec60f3c281296a8
2023-anik <cse2023anik@gmail.com>
2024-06-23 13:29:11 +0600

    All project updated

--- Basic_Codes/Data_Structure/create_node.cpp
+++ Basic_Codes/Data_Structure/create_node.cpp
@@ -2,20 +2,43 @@
 using namespace std;
 #define endl '\n'
 
+// class Node{
+//     public:
+//     int val;
+//     Node* next;
+// };
+
+//using a constractor
 class Node{
     public:
     int val;
     Node* next;
+
+    /****constractor****/
+    Node(int val){
+        this->val=val;
+        this->next=NULL;
+    }
+    /*******************/
 };
 
 int main(){
-    Node a, b;
-    a.val=10;
-    b.val=20;
+    // Node a, b;
+    // a.val=10;
+    // b.val=20;
+    // a.next=&b;
+    // b.next=NULL;
+    // cout<<a.val<<endl;
+    // cout<<a.next->val<<endl;
+    // cout<<(*a.next).val<<endl;
+
+    //Using constructor
+    Node a(10), b(20);
     a.next=&b;
     b.next=NULL;
     cout<<a.val<<endl;
-    cout<<a.next->val<<endl;
     cout<<(*a.next).val<<endl;
+    cout<<a.next->val<<endl;
+    //cout<<a->next->val<<endl; --->logically doesn't make sense
     return 0;
 }
