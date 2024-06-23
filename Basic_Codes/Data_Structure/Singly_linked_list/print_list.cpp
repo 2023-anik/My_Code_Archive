7f6a3b2f6b20ff2df01a1be03aeb923a1f0ce054
2023-anik <cse2023anik@gmail.com>
2024-06-23 14:50:16 +0600

    add print list file

---
+++ Basic_Codes/Data_Structure/Singly_linked_list/print_list.cpp
@@ -0,0 +1,48 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define endl '\n'
+
+class Node{
+    public:
+    int val;
+    Node* next;
+
+    /****constractor****/
+    Node(int val){
+        this->val=val;
+        this->next=NULL;
+    }
+    /*******************/
+};
+
+/***function for print all list's value***/
+void print_list(Node* head){
+    Node* temp=head;
+    while(temp!=NULL){
+        cout<<temp->val<<endl;
+        temp = temp->next;
+    }
+}
+
+int main(){
+    Node* head = new Node(10);
+    Node* a = new Node(20);
+    Node* b = new Node(30);
+    Node* c = new Node(40);
+    Node* d = new Node(50);
+
+    head->next=a;
+    a->next=b;
+    b->next=c;
+    c->next=d;
+    d->next=NULL;
+
+    // cout<<head->val<<endl;                        // (*head).val=10
+    // cout<<head->next->val<<endl;                  // (*(*head).next).val=20
+    // cout<<head->next->next->val<<endl;            // (*(*(*head).next).next).val=30
+    // cout<<head->next->next->next->val<<endl;      // (*(*(*(*head).next).next).next).val=40
+    // cout<<head->next->next->next->next->val<<endl;// (*(*(*(*(*head).next).next).next).next)=50
+    
+    print_list(head);
+    return 0;
+}
