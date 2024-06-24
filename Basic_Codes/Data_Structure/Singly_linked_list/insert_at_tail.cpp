f9b0610dec127414ba3d4312aa3cc36f332fc34d
2023-anik <cse2023anik@gmail.com>
2024-06-24 20:48:49 +0600

    All updated

--- Basic_Codes/Data_Structure/Singly_linked_list/insert_at_tail.cpp
+++ Basic_Codes/Data_Structure/Singly_linked_list/insert_at_tail.cpp
@@ -20,7 +20,7 @@ void insert_at_tail(Node* &head, int &x){
     Node* newNode=new Node(x);
     if(head==NULL){
         head=newNode;
-        cout<<"Successfully inserted:)"<<endl;
+        cout<<"Successfully inserted:)"<<endl<<endl;
         return;
     }
     Node* temp=head;
@@ -28,10 +28,48 @@ void insert_at_tail(Node* &head, int &x){
         temp=temp->next;
     temp->next=newNode;
     newNode->next=NULL;
-    cout<<"Successfully inserted:)"<<endl;
+    cout<<"Successfully inserted:)"<<endl<<endl;
+}
+
+void print_the_lists(Node* head){
+    cout<<"Your list: ";
+    Node* temp=head;
+    while(temp!=NULL){
+        cout<<temp->val<<" ";
+        temp=temp->next;
+    }
 }
 
 int main(){
     Node* head=NULL;
+    int cmd;
+    do{
+        cout<<"0: Exit"<<endl;
+        cout<<"1: Enter a value to insert at tail"<<endl;
+        cout<<"2: See your lists"<<endl;
+        cout<<"Type any from 0 to 2: ";
+        cin>>cmd;
+        cout<<endl;
+        switch(cmd){
+            case 0:{
+                cout<<"Successfully Exit:)"<<endl;
+                return 0;
+            }
+            case 1:{
+                int value;
+                cout<<"Enter your value: ";
+                cin>>value;
+                insert_at_tail(head, value);
+                cout<<endl;
+                break;
+            }
+            case 2:{
+                print_the_lists(head);
+                cout<<endl<<endl;
+                break;
+            }
+        }
+    }while(cmd!=0);
+    
     return 0;
 }
