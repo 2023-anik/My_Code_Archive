c36035b94c3a3e6b0e297c810ea7af9675074b52
2023-anik <cse2023anik@gmail.com>
2024-06-26 00:55:30 +0600

    Refactor delete_from_any_position.cpp for improved deletion at any position

--- Basic_Codes/Data_Structure/Singly_linked_list/error_handle.cpp
+++ Basic_Codes/Data_Structure/Singly_linked_list/error_handle.cpp
@@ -60,6 +60,76 @@ void insert_at_any_position(Node* &head, int position, int value){
     cout<<"Successfully inserted at point "<<position<<endl<<endl;
 }
 
+//to delete from head
+void delete_from_head(Node* &head){
+    if(head==nullptr){
+        cout<<"Already Empty List"<<endl<<endl;
+        return;
+    }
+    Node* deleteNode=head;
+    head=head->next;
+    delete deleteNode;
+    cout<<"Successfully have deleted from the head"<<endl<<endl;
+}
+
+//to delete from tail
+void delete_from_tail(Node* &head){
+    if(head==nullptr){
+        cout<<"Already empty"<<endl<<endl;
+        return;
+    }
+    if(head->next==nullptr){
+        delete head;
+        head=nullptr;
+        cout<<"Successfully have deleted from the tail"<<endl<<endl;
+        return;
+    }
+    Node* temp=head;
+    while(temp->next not_eq nullptr)
+        temp=temp->next;
+    delete temp->next;
+    temp->next=nullptr;
+    cout<<"Successfully have deleted from the tail"<<endl<<endl;
+}
+
+//to delete from any position
+void delete_from_any_position(Node* &head, int position){
+    if(position<1){
+        cout<<"Invalid position"<<endl<<endl;
+        return;
+    }
+    if(position==1){
+        Node* deleteNode=head;
+        head=head->next;
+        delete deleteNode;
+        cout<<"Successfully have delete from position "<<position<<endl<<endl;
+        return;
+    }
+    Node* temp=head;
+    for(int i=1; i<position-1; ++i){
+        if(temp==nullptr){
+            cout<<"Invalid position"<<endl<<endl;
+            return;
+        }
+        temp=temp->next;
+    }
+    Node* deleteNode=temp->next;
+    temp->next=temp->next->next;
+    delete deleteNode;
+    cout<<"Successfully have deleted from position "<<position<<endl<<endl;    
+}
+
+//to count list size
+int list_size(Node* &head){
+    Node* temp=head;
+    int size=0;
+    while(temp not_eq nullptr){
+        size++;
+        temp=temp->next;
+    }
+    return size;
+}
+
 //to print to list
 void print_the_list(Node* &head){
     cout<<"Elements of the list: ";
@@ -72,13 +142,15 @@ void print_the_list(Node* &head){
 int main(){
     Node* head=NULL;
     int cmd;
-    do{
+    while(1){
         cout<<"[1] Insert at head"<<endl;//Error Handling Done
         cout<<"[2] Insert at tail"<<endl;//Error Handling Done
         cout<<"[3] Insert at any position"<<endl;//Error Handling Done
-        cout<<"[4] Delete from head"<<endl;
-        cout<<"[5] Delete from any position"<<endl;
-        cout<<"[6] See the list"<<endl;
+        cout<<"[4] Delete from head"<<endl;//Error Handling Done
+        cout<<"[5] Delete from tail"<<endl;//Error Handling Done
+        cout<<"[6] Delete from any position"<<endl;//Error Handling Done
+        cout<<"[7] See the list size"<<endl;
+        cout<<"[8] See the list"<<endl;
         cout<<"[0] Exit"<<endl;
         cout<<"Enter any from 0 to 6 ";
         cin>>cmd;
@@ -107,12 +179,24 @@ int main(){
                 break;
             }
             case 4:{
+                delete_from_head(head);
                 break;
             }
             case 5:{
+                delete_from_tail(head);
                 break;
             }
             case 6:{
+                int posi;
+                cout<<"Enter position: "; cin>>posi;
+                delete_from_any_position(head, posi);
+                break;
+            }
+            case 7:{
+                cout<<list_size(head)<<endl<<endl;
+                break;
+            }
+            case 8:{
                 print_the_list(head);
                 break;
             }
@@ -122,7 +206,6 @@ int main(){
             }
             default:cout<<"Invalid enter! Try again."<<endl<<endl;
         }
-
-    }while(1);
+    }
     return 0;
 }
