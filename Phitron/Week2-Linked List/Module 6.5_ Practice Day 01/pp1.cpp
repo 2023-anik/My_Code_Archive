39cf1de65f1b03ace740f25b97fcf62bf7120c16
2023-anik <cse2023anik@gmail.com>
2024-06-26 01:48:24 +0600

    Added pp1

---
+++ Phitron/Week2-Linked List/Module 6.5_ Practice Day 01/pp1.cpp
@@ -0,0 +1,50 @@
+/* Take a singly linked list as input and
+print the size of the linked list.*/
+
+#include<bits/stdc++.h>
+using namespace std;
+#define endl '\n'
+
+class Node{
+    public:
+    int val;
+    Node* next;
+
+    /****constractor****/
+    Node(int val){
+        this->val=val;
+        this->next=nullptr;
+    }
+};
+
+int list_size(Node* &head){
+    Node* temp=head;
+    int size=0;
+    while(temp not_eq nullptr){
+        size++;
+        temp=temp->next;
+    }return size;
+}
+
+void insert_at_tail(Node* &head, int value){
+    Node* newNode=new Node(value);
+    if(head == nullptr){
+        head=newNode;
+        return;
+    }
+    Node* temp=head;
+    while(temp->next not_eq nullptr)
+        temp=temp->next;
+    temp->next=newNode;
+}
+
+int main(){
+    Node* head=nullptr;
+    int value;
+    while(cin>>value){
+        if(value == -1) break;
+        insert_at_tail(head, value);
+    }
+    cout<<list_size(head)<<endl;
+    return 0;
+}
