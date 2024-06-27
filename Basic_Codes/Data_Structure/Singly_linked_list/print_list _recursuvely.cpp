603ddec7f0e210021b9b3f5d4838fc11232a1965
Anik Sen <cse2023anik@gmail.com>
2024-06-27 12:38:59 +0600

    Updated

---
+++ Basic_Codes/Data_Structure/Singly_linked_list/print_list _recursuvely.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define endl '\n'
+
+class Node{
+    public:
+    int val;
+    Node* next;
+    Node(int val){
+        this->val=val;
+        this->next=NULL;
+    }
+};
+
+//recursivly print the list
+void print_list(Node* head){
+    if(head==nullptr) return;
+    cout<<head->val<<' ';
+    print_list(head->next);
+}
+
+int main(){
+    Node* head=new Node(10);
+    Node* a=new Node(20);
+    Node* b=new Node(39);
+    Node* c=new Node(40);
+    head->next=a;
+    a->next=b;
+    b->next=c;
+    print_list(head);
+    cout<<endl;
+    return 0;
+}
