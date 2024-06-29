3baea636acce23276d37e56b6ed1e1f4aebf60bf
2023-anik <cse2023anik@gmail.com>
2024-06-29 14:37:22 +0600

    refactor: Improve uniqueness testing using selection sort

--- Basic_Codes/Algorithm/Sorting Algo/Selection Sort/Uniqueness_testing.cpp
+++ Basic_Codes/Algorithm/Sorting Algo/Selection Sort/Uniqueness_testing.cpp
@@ -2,18 +2,23 @@
 using namespace std;
 #define endl '\n'
 
-bool unique(int *arr, int size){
-    for(int i=size-1; i>=0; --i)
-        for(int j=0; j<i; ++j)
-            if(arr[i]==arr[j])
+bool uniqueUsingSelectionSort(int *arr, int size){
+    for(int i=0; i<size-1; ++i){
+        int minIdx=i;
+        for(int j=size-1; j>=0; --j){
+            if(arr[j]==arr[minIdx])
                 return false;
-    return true;
+            if(arr[minIdx]>arr[j])
+                minIdx=j;
+        }
+        swap(arr[minIdx], arr[i]);
+    }return true;
 }
 
 int main(){
     int arr[]={37, 10, 14, 29, 13};
-    cout<<((unique(arr, 5))?"YES":"NO")<<endl;
+    cout<<((uniqueUsingSelectionSort(arr, 5))?"YES":"NO")<<endl;
     int arr1[]={37, 10, 10, 29, 13};
-    cout<<((unique(arr1, 5))?"YES":"NO")<<endl;
+    cout<<((uniqueUsingSelectionSort(arr1, 5))?"YES":"NO")<<endl;
     return 0;
 }
