71e9041f565e79fca8d46e4a5d17f4cb3099b18a
2023-anik <cse2023anik@gmail.com>
2024-06-29 18:02:06 +0600

    refactor: Improve naming convention for uniqueness testing function

--- Basic_Codes/Algorithm/Sorting Algo/Selection Sort/Uniqueness_testing.cpp
+++ Basic_Codes/Algorithm/Sorting Algo/Selection Sort/Uniqueness_testing.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 #define endl '\n'
 
-bool uniqueUsingSelectionSort(int *arr, int size){
+bool unique_Using_Selection_Sort(int *arr, int size){
     for(int i=0; i<size-1; ++i){
         int minIdx=i;
         for(int j=size-1; j>=0; --j){
@@ -17,8 +17,8 @@ bool uniqueUsingSelectionSort(int *arr, int size){
 
 int main(){
     int arr[]={37, 10, 14, 29, 13};
-    cout<<((uniqueUsingSelectionSort(arr, 5))?"YES":"NO")<<endl;
+    cout<<((unique_Using_Selection_Sort(arr, 5))?"YES":"NO")<<endl;
     int arr1[]={37, 10, 10, 29, 13};
-    cout<<((uniqueUsingSelectionSort(arr1, 5))?"YES":"NO")<<endl;
+    cout<<((unique_Using_Selection_Sort(arr1, 5))?"YES":"NO")<<endl;
     return 0;
 }
