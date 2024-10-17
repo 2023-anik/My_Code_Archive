699ef1c0d6ed0d935f0f0a875f53f23051f99be3
Anik Sen <cse2023anik@gmail.com>
2024-10-18 01:40:00 +0600

    Refactor code to disable error squiggles in C/C++ files

---
+++ Dynamic Programming/test.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+using ll = long long;
+#define endl '\n'
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define FOR(i, n) for (int i = 0; i < n; ++i)//0 to n-1
+#define FOR1(i, n) for (int i = 1; i <= n; ++i)//1 to n
+#define REP(i, a, b) for (int i = a; i <= b; ++i)//a to b inclusive
+#define FAST ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+typedef vector<ll> vi;
+typedef pair<ll, ll> pii;
+
+void solve(){
+    cout<<"this is the fuck'in code for forever"<<endl;
+}
+
+int32_t main(){
+    FAST
+    int t=1;
+    // cin>>t;
+    while(t--) solve();
+    return 0;
+}
