79b6d5a51478521575a6469accc5fcad57df4c69
Anik Sen <cse2023anik@gmail.com>
2025-01-03 22:08:45 +0600

    Add new problem implementations for set, multiset, and priority queues; include logic and input handling

--- XPSC- 5/week 2/03 Jan 25/w2 (topicwise)/p3 (Epic Transformation).cpp
+++ XPSC- 5/week 2/03 Jan 25/w2 (topicwise)/p3 (Epic Transformation).cpp
@@ -15,36 +15,41 @@ typedef pair<int, int> pii;
 void solve(){
     int n;
     cin >> n;
-    vi freq(n + 1, 0);
-    for (int i = 0; i < n; i++) {
-        int x;
+    map<int, int> mp;
+    int x;
+    while (n--){
         cin >> x;
-        freq[x]++;
+        mp[x]++;
     }
+    priority_queue<int> pq;//based on max heap
 
-    priority_queue<int> pq;
-    for (int i = 1; i <= n; i++) {
-        if (freq[i] > 0) {
-            pq.push(freq[i]);
-        }
-    }
+    for(auto &[x, y]:mp)
+        pq.push(y);
 
-    while (pq.size() > 1) {
-        int top1 = pq.top();
+    int top1, top2;
+    while (!pq.empty()){
+        if(pq.size()<2)
+            break;
+            
+        top1 = pq.top();
         pq.pop();
-        int top2 = pq.top();
+        top2 = pq.top();
         pq.pop();
-        top1--;
-        top2--;
-        if (top1 > 0) pq.push(top1);
-        if (top2 > 0) pq.push(top2);
-    }
 
-    if (pq.empty()) {
-        cout << 0 << endl;
-    } else {
-        cout << pq.top() << endl;
+        top1--, top2--;
+
+        if(top1>=1)
+            pq.push(top1);
+        if(top2>=1)
+            pq.push(top2);
     }
+    int ans = 0;
+    while(!pq.empty()){
+        ans += pq.top();
+        pq.pop();
+    }
+    cout << ans << endl;
+    return;
 }
 
 int32_t main(){
