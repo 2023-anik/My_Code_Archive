7a2f6352e116c7deac0485403f03ce48aede0ec9
Anik Sen <cse2023anik@gmail.com>
2025-01-04 14:15:10 +0600

    Add initial implementation for "Train and Queries" problem; set up input handling and main function structure

---
+++ XPSC- 5/week 2/03 Jan 25/w2 (topicwise)/p4 (Train and Queries).cpp
@@ -0,0 +1,40 @@
+//https://codeforces.com/problemset/problem/1702/C
+
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define FAST ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define no cout<<"NO"<<'\n'
+#define yes cout<<"YES"<<'\n'
+#define endl '\n'
+typedef vector<int> vi;
+typedef pair<int, int> pii;
+
+void solve(){
+    int n, k;
+    cin >> n >> k;
+    map<int, multiset<int>> mp;
+    int i, x;
+    for (i = 1; i<=n; i++){
+        cin >> x;
+        mp[x].insert(i);
+    }
+    int a, b;
+    while (k--){
+        cin >> a >> b;
+        if(mp.find(a)!=mp.end() && mp.find(b)!=mp.end())
+            cout << (*mp[a].begin() < *mp[b].rbegin() ? "YES" : "NO") << endl;
+        else
+            cout << "NO" << endl;
+    }
+}
+
+int32_t main(){
+    FAST
+    int t=1;
+    cin>>t;
+    while(t--) solve();
+    return 0;
+}
