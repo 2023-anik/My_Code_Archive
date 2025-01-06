207749cc3dc1719733919f61b884089f1d1152dd
Anik Sen <cse2023anik@gmail.com>
2025-01-07 00:52:58 +0600

    Refactor logic in "D (Anti Adjacent Swaps)" to simplify conditions and improve readability

--- XPSC- 5/6 Jan 25/practice contest 7/D (Anti Adjacent Swaps).cpp
+++ XPSC- 5/6 Jan 25/practice contest 7/D (Anti Adjacent Swaps).cpp
@@ -1,14 +1,11 @@
 #include<bits/stdc++.h>
 using namespace std;
 #define int long long
-#define all(x) (x).begin(), (x).end()
-#define rall(x) (x).rbegin(), (x).rend()
 #define FAST ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
 #define no cout<<"NO"<<'\n'
 #define yes cout<<"YES"<<'\n'
 #define endl '\n'
 typedef vector<int> vi;
-typedef pair<int, int> pii;
 
 void solve(){
     int n;
@@ -18,31 +15,23 @@ void solve(){
         cin >> it;
 
     if(n==1){
-        no;
+        yes;
         return;
     }
-
-    vi e, o;
-    int i;
-    for (i = 0; i<n; i++)
-        if(i%2==0)
-            e.push_back(a[i]);
+    if(n==2){
+        if(a[0]<=a[1])
+            yes;
         else
-            o.push_back(a[i]);
-
-    sort(all(e));
-    sort(all(o));
-    int ie = 0, io = 0;
-    for (i = 0; i < n; i++)
-        if(i%2==0)
-            a[i] = e[ie++];
-        else
-            a[i] = o[io++];
-    for (i = 0; i < n-1; i++)
-        if (a[i] > a[i+1]){
             no;
-            return;
-        }
+        return;
+    }
+    if(n==3){
+        if(a[0]<=a[1] && a[1]<=a[2] || a[0]>=a[1] && a[1]>=a[2])
+            yes;
+        else
+            no;
+        return;
+    }
     yes;
 }
 
