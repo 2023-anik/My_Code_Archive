fe6e206d25bc0490f1bb1dc3a5580b3e01e1c5b0
Anik Sen <cse2023anik@gmail.com>
2025-01-09 22:46:45 +0600

    Refactor "print SubSets" implementation; improve output formatting and add sorting for subsets

--- XPSC- 5/Week 6/BitMask/print SubSets.cpp
+++ XPSC- 5/Week 6/BitMask/print SubSets.cpp
@@ -1,13 +1,34 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-int main(){
+int main()
+{
     vector<int> a = {1, 2, 3, 4, 5};
+    sort(a.rbegin(), a.rend());
     int i, mask;
-    for (mask = 0; mask < (1 << 5); mask++){
-        // cout << mask << "->" << (1 << 5) << endl;
-        for (i = 0; i < 5; i++){
-            if((mask>>i)&1){//check the bit on or not
+    cout << "Index |  Bitmask  | Subsets" << endl;
+    cout << "------|-----------|----------" << endl;
+    for (mask = 0; mask < (1 << 5); mask++)
+    {
+        cout << "    " << mask << "  ";
+        for (i = 4; i >= 0; i--)
+        {
+            if ((mask >> i) & 1)
+            { // check the bit on or not
+                cout << 1 << " ";
+            }
+            else
+            {
+                cout << 0 << " ";
+            }
+        }
+        cout << "     ";
+        if (mask == 0)
+            cout << "{NULL}";
+        for (i = 0; i < 5; i++)
+        {
+            if ((mask >> i) & 1)
+            { // check the bit on or not
                 cout << a[i] << " ";
             }
         }
