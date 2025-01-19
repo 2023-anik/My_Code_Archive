730a2a5ce82bf1a38717b0f23764cf64a80227df
2023-anik <cse2023anik@gmail.com>
2025-01-19 10:50:56 +0600

    Add solutions for local problems: first and last occurrence of key with implementations and test cases

---
+++ XPSC- 5/Week 8/first occurrence of key.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define endl '\n'
+
+int firstOccurrence(vector<int>&a, int n, int key){
+    int l=0, r=n-1, mid, ans=-1;
+    while(l<=r){
+        mid=l+(r-l)/2;
+        if(key==a[mid]){
+            ans = mid;
+            r=mid-1;
+        }else if(key>a[mid]){
+            l=mid+1;
+        }
+        else{
+            r=mid-1;
+        }
+    }
+    return ans;
+}
+
+int main(){
+    vector<int>a={2, 4, 6, 6, 7, 8, 8, 8, 10, 12};
+    cout<<firstOccurrence(a, 10, 8)<<endl;
+}
---
+++ XPSC- 5/Week 8/last occurrence of key.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define endl '\n'
+
+int lastOccurrece(vector<int>a, int n, int key){
+    int l=0, r=n-1, mid, ans=-1;
+    while(l<=r){
+        mid=l+(r-l)/2;
+        if(key==a[mid]){
+            ans = mid;
+            l=mid+1;
+        }else if(key>a[mid]){
+            l=mid+1;
+        }else{
+            r=mid-1;
+        }
+    }
+    return ans;//0 besed index
+}
+
+int main(){
+    vector<int>a={2, 4, 6, 6, 7, 8, 8, 8, 10, 12};
+    cout<<lastOccurrece(a, 10, 8)<<endl;
+}
