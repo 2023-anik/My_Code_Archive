f3c5ceabee562d38286f7effc89a9af2a7efbbde
2023-anik <cse2023anik@gmail.com>
2025-01-23 10:47:45 +0600

    Refactor Fibonacci implementation using dynamic programming and improve input handling

--- Dynamic Programming/fibonacci using dp(top down approach).cpp
+++ Dynamic Programming/fibonacci using dp(top down approach).cpp
@@ -1,40 +1,27 @@
 #include<bits/stdc++.h>
 using namespace std;
-#define int long long
-#define all(x) (x).begin(), (x).end()
-#define rall(x) (x).rbegin(), (x).rend()
-#define FOR(i, n) for (int i = 0; i < n; ++i)//0 to n-1
-#define FOR1(i, n) for (int i = 1; i <= n; ++i)//1 to n
-#define REP(i, a, b) for (int i = a; i <= b; ++i)//a to b inclusive
-#define FAST ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
-#define no cout<<"NO"<<'\n'
-#define yes cout<<"YES"<<'\n'
 #define endl '\n'
-typedef vector<int> vi;
-typedef pair<int, int> pii;
+#define FAST ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+const int N=1e6+5;
 
-const int N = 1e6+5;
-vi dp(N, -1);
+vector<int> dp(N,-1);
 
+// Top Down Approach
 int fibo(int n){
-    if(n==0 || n==1) return n;
-    if(dp[n] != -1) return dp[n];
-    // return fibo(n-1) + fibo(n-2);
-    // int ans = fibo(n-1) + fibo(n-2);
-    // dp[n] = ans;
-    // return ans;
-    dp[n] = fibo(n - 1) + fibo(n - 2);
-    return fibo(n - 1) + fibo(n - 2);
+    if(n==0 || n==1){
+        return n;
+    }
+    if(dp[n]!=-1){
+        return dp[n];
+    }
+    dp[n]=fibo(n-1)+fibo(n-2);
+    return dp[n];
 }
 
-void solve(){
-    cout<<fibo(1000)<<endl;
-}
-
-int32_t main(){
+int main(){
     FAST
-    int t=1;
-    // cin>>t;
-    while(t--) solve();
+    int n;
+    cin>>n;
+    cout<<fibo(n)<<endl;
     return 0;
 }
