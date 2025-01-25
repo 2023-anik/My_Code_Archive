a2221d849f3a8b3beb5cf85c76e43f8c8739d204
2023-anik <cse2023anik@gmail.com>
2025-01-25 17:45:35 +0600

    Implement bottom-up dynamic programming approach for subset sum problem and initialize DP table

--- Dynamic Programming/Knapsack variation/subset sum(bottom up).cpp
+++ Dynamic Programming/Knapsack variation/subset sum(bottom up).cpp
@@ -5,10 +5,22 @@ using namespace std;
 
 int n, s;
 vector<int>a;
-int dp[1005][1005];
+bool dp[1005][1005];
 
+//bottom up approach
 bool subsetSum(){
     int i, j;
+    for(i=1; i<=n; i++){
+        for(j=0; j<=s; j++){
+            if(a[i-1]>j){
+                dp[i][j]=dp[i-1][j];
+            }
+            else{
+                dp[i][j]=dp[i-1][j-a[i-1]] || dp[i-1][j];
+            }
+        }
+    }
+    return dp[n][s];
 }
 
 int main(){
@@ -20,6 +32,10 @@ int main(){
         a.push_back(x);
     }
     cin>>s;
+    dp[0][0]=1;
+    for(int i=1; i<=s; i++){
+        dp[0][i]=0;
+    }
     cout<<(subsetSum()?"YES":"NO")<<endl;
     return 0;
 }
