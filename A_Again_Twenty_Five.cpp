cc04e3114aa11747286cbd522024e94eaab051fd
2023-anik <cse2023anik@gmail.com>
2025-02-12 18:01:46 +0600

    Add solution for Codeforces problem "A. Again Twenty Five!" with basic input/output handling

---
+++ A_Again_Twenty_Five.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+using ll = long long;
+#define endl '\n'
+#define yes cout << "YES" << endl
+#define no cout << "NO" << endl
+#define all(v) v.begin(), v.end()
+#define allr(v) v.rbegin(), v.rend()
+#define input(v) for(auto &i : v) cin >> i
+
+void solve(){
+    ll n;
+    cin>>n;
+    cout<<25<<endl;
+}
+
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    int t;
+    t = 1;
+    // cin>>n;
+    while(t--){
+        solve();
+    }
+    return 0;
+}
